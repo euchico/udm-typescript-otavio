e3bd2ce4e9c0c208f9ef059a1b6ff9c1d34a40fe
euchico <franciscodepaula.web@gmail.com>
2025-06-03 00:00:07 -0300

    Aulas 5 e 6.

---
+++ aula-06/index.js
@@ -0,0 +1,12 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case sensitive
+// NÃO UTILIZE VAR, UTILIZE LET.
+
+let nomeCliente = 'Francisco';
+let nomecliente = 'de Paula';
+
+console.log(nomeCliente, nomecliente);
